3b8778b32fad7a173e857381a41c7c9b0ec3e992
Andrew Polyansky <andrew.polyansky@deip.world>
2021-03-11 15:49:41 +0300

    add project create and update tests

--- pallets/deip/src/tests.rs
+++ pallets/deip/src/tests.rs
@@ -38,7 +38,7 @@ fn cant_add_duplicate_domain() {
 fn add_project() {
 	new_test_ext().execute_with(|| {
 		let domain = Domain::random();
-		let account_id = 2;
+		let account_id: u64 = 2;
 		let project_id = ProjectId::random();
 		
 		assert_ok!(Deip::add_domain(Origin::signed(1), domain.clone()));
@@ -53,6 +53,15 @@ fn add_project() {
 		};
 		
 		assert_ok!(Deip::create_project(Origin::signed(1), project.clone()));
+
+		
+		// TODO Add event check
+		// let expected_event = mock::Event::pallet_deip(crate::Event::ProjectCreated(account_id, project)::<u64, Project<H256, u64>>);
+
+		// assert_eq!(
+		// 	System::events()[0].event,
+		// 	expected_event,
+		// );
 
 		let projects = Projects::<Test>::get();
 		let project_stored = ProjectMap::<Test>::get(project_id);
@@ -101,3 +110,110 @@ fn cant_add_project_with_non_exixsted_domain() {
 	})
 }
 
+#[test]
+fn cant_add_duplicated_project() {
+	new_test_ext().execute_with(|| {
+		let domain = Domain::random();
+		let account_id = 2;
+		let project_id = ProjectId::random();
+		
+		assert_ok!(Deip::add_domain(Origin::signed(1), domain.clone()));
+
+		let project = Project {
+			is_private: false,
+			external_id: project_id,
+			team_id: account_id,
+			description: H256::random(),
+			domains: vec![domain],
+			members: vec![account_id],
+		};
+		
+		assert_ok!(Deip::create_project(Origin::signed(1), project.clone()));
+
+		assert_noop!(
+			Deip::create_project(Origin::signed(1), project.clone()),
+			Error::<Test>::ProjectAlreadyExists
+		);
+
+	})
+}
+
+
+#[test]
+fn update_project() {
+	new_test_ext().execute_with(|| {
+		let domain = Domain::random();
+		let account_id: u64 = 1;
+		let project_id = ProjectId::random();
+		
+		assert_ok!(Deip::add_domain(Origin::signed(1), domain.clone()));
+
+		let project = Project {
+			is_private: false,
+			external_id: project_id,
+			team_id: account_id,
+			description: H256::random(),
+			domains: vec![domain],
+			members: vec![account_id],
+		};
+
+		let new_description = H256::random();
+		let new_members = vec![1,2];
+		
+		assert_ok!(Deip::create_project(Origin::signed(1), project.clone()));
+
+		assert_ok!(Deip::update_project(Origin::signed(1), project_id, Some(new_description), Some(true), Some(new_members.clone())));
+
+
+		let project_stored = ProjectMap::<Test>::get(project_id);
+
+		assert_eq!(project_stored.description, new_description);
+		assert_eq!(project_stored.is_private, true);
+		assert_eq!(project_stored.members, new_members);
+
+
+	})
+}
+
+
+#[test]
+fn cant_update_project_not_belonged_to_your_signature() {
+	new_test_ext().execute_with(|| {
+		let domain = Domain::random();
+		let account_id: u64 = 2;
+		let project_id = ProjectId::random();
+		
+		assert_ok!(Deip::add_domain(Origin::signed(1), domain.clone()));
+
+		let project = Project {
+			is_private: false,
+			external_id: project_id,
+			team_id: account_id,
+			description: H256::random(),
+			domains: vec![domain],
+			members: vec![account_id],
+		};
+
+		let new_description = H256::random();
+		let new_members = vec![1,2];
+		
+		assert_ok!(Deip::create_project(Origin::signed(1), project.clone()));
+
+		assert_noop!(
+			Deip::update_project(Origin::signed(1), project_id, Some(new_description), Some(true), Some(new_members.clone())),
+			Error::<Test>::NoPermission
+		);
+	})
+}
+
+#[test]
+fn cant_update_not_existed_project() {
+	new_test_ext().execute_with(|| {
+		let project_id = ProjectId::random();
+
+		assert_noop!(
+			Deip::update_project(Origin::signed(1), project_id, None, None, None),
+			Error::<Test>::NoSuchProject
+		);
+	})
+}
