0f3efdf0a1b85ee92ea1f08ba409e0bf576061d6
Georgy Shepelev <geo.shepelev@gmail.com>
2021-08-05 14:26:46 +0400

    add prefix in org_key funcs

--- pallets/deip_org/src/lib.rs
+++ pallets/deip_org/src/lib.rs
@@ -275,18 +275,18 @@ pub mod pallet {
     
     impl<T: Config> Pallet<T> {
         pub fn org_key(org_name: &OrgName) -> T::AccountId {
-            let entropy = (org_name.as_bytes()).using_encoded(sp_io::hashing::blake2_256);
+            let entropy = (b"deip/DAOs/", org_name.as_bytes()).using_encoded(sp_io::hashing::blake2_256);
             T::AccountId::decode(&mut &entropy[..]).unwrap_or_default()
         }
     }
 
     pub fn org_key2<T: frame_system::Config>(org_name: &OrgName) -> T::AccountId {
-        let entropy = (org_name.as_bytes()).using_encoded(sp_io::hashing::blake2_256);
+        let entropy = (b"deip/DAOs/", org_name.as_bytes()).using_encoded(sp_io::hashing::blake2_256);
         T::AccountId::decode(&mut &entropy[..]).unwrap_or_default()
     }
     
     pub fn org_key3<T: Decode + Default>(org_name: &OrgName) -> T {
-        let entropy = (org_name.as_bytes()).using_encoded(sp_io::hashing::blake2_256);
+        let entropy = (b"deip/DAOs/", org_name.as_bytes()).using_encoded(sp_io::hashing::blake2_256);
         T::decode(&mut &entropy[..]).unwrap_or_default()
     }
     
