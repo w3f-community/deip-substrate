285e573d7e0b50791e471984eda715ed10120079
yaro <0623forbidden@gmail.com>
2021-08-09 12:22:37 +0300

    (event-proxy): Add broker abstraction

--- event-proxy/src/main.rs
+++ event-proxy/src/main.rs
@@ -3,6 +3,7 @@ mod events;
 mod types;
 mod runtime;
 mod call_serializer;
+mod broker;
 
 use substrate_subxt::ClientBuilder;
 use substrate_subxt::NodeTemplateRuntime;
@@ -31,11 +32,17 @@ async fn main() {
         sub,
         events_decoder
     );
+    
+    let (control_tx, control_rx) = tokio::sync::mpsc::channel(1);
+    let _broker_handle = tokio::spawn(async move { broker::broker_loop(control_rx) });
+    
     loop {
         while let Some(Ok(e)) = sub.next().await {
             log::debug!("{:?} ; {:?} ; {:?}", e.variant, e.module, e.data);
             let k = known_events::<RuntimeT>(&e);
-            println!("{}", serde_json::to_string_pretty(&k).unwrap());
+            let payload = serde_json::to_string_pretty(&k).unwrap();
+            println!("{}", &payload);
+            control_tx.send(broker::Send(payload)).await.unwrap_or_default();
         }
     }
 }
