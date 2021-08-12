3ffe7566e6f91188e5ec1463a62048e67d56c5fe
yaro <0623forbidden@gmail.com>
2021-08-12 15:06:28 +0300

    (event-proxy): Refine main flow-control

--- event-proxy/src/main.rs
+++ event-proxy/src/main.rs
@@ -13,6 +13,7 @@ use substrate_subxt::NodeTemplateRuntime;
 use substrate_subxt::{EventSubscription};
 
 use tokio::sync::mpsc;
+use futures::stream::FuturesOrdered;
 
 use events::*;
 use types::register_types;
@@ -35,22 +36,28 @@ async fn main() {
     
     flexi_logger::Logger::try_with_env().unwrap().start().unwrap();
     
+    // Init rpc-client-builder-actor:
     let mut client_builder_actor = RpcClientBuilderActor;
-    let (client_builder_io1, mut client_builder_io2)
+    let (cb_io, mut cb_io2)
         = RpcClientBuilderActorIO::pair();
-    let _client_builder_task = tokio::spawn(async move {
-        client_builder_actor.actor_loop(client_builder_io1).await
+    tokio::spawn(async move {
+        client_builder_actor.actor_loop(cb_io).await
     });
-    client_builder_io2.send(ActorDirective::Input(())).await.unwrap();
-    let client = client_builder_io2.recv().await.unwrap().unwrap();
     
+    // Get rpc-client:
+    cb_io2.send(ActorDirective::Input(())).await.unwrap();
+    let client = cb_io2.recv().await.unwrap().unwrap();
+    
+    // Init rpc-client-status-actor:
     let mut rpc_client_status = RpcClientStatusActor::new(client.rpc_client().clone());
-    let (cs_io1, mut cs_io2) 
+    let (cs_io, mut cs_io2) 
         = RpcClientStatusActorIO::pair();
-    tokio::spawn(async move { rpc_client_status.actor_loop(cs_io1).await });
+    tokio::spawn(async move {
+        rpc_client_status.actor_loop(cs_io).await
+    });
     
+    // Spawn check_disconnect periodic for rpc-client-status-actor:
     let (mut cs_i2, mut cs_o2) = cs_io2.split();
-    
     tokio::spawn(async move {
         loop {
             let x = cs_o2.send(RpcClientStatusActorInputData::check_disconnect()).await;
@@ -59,22 +66,44 @@ async fn main() {
         }
     });
     
+    // Init blockchain-actor:
     let mut blockchain = BlockchainActor::new(client);
-    let (b_io1, mut b_io2) = BlockchainActorIO::pair();
-    tokio::spawn(async move { blockchain.actor_loop(b_io1).await });
+    let (b_io, mut b_io2) = BlockchainActorIO::pair();
+    tokio::spawn(async move {
+        blockchain.actor_loop(b_io).await
+    });
+    
+    // Get block-subscription:
     b_io2.send(BlockchainActorInputData::subscribe_finalized_blocks()).await.unwrap();
     let subscription = b_io2.recv().await.unwrap();
-    match subscription {
-        BlockchainActorOutput::SubscribeFinalizedBlocks(Ok(mut s)) => {
-            loop {
-                let header = s.next().await.unwrap().unwrap();
-                let block = fetch_block(header.number, &mut b_io2).await;
-                println!("BLOCK: {:?}", &block);
-            }
-        },
+    let mut sub = match subscription {
+        BlockchainActorOutput::SubscribeFinalizedBlocks(Ok(s)) => s,
         _ => unreachable!(),
-    }
+    };
     
+    // Init message-broker-actor:
+    let mut message_broker = MessageBrokerActor::new();
+    let (mb_io, mb_io2) = MessageBrokerActorIO::pair();
+    tokio::spawn(async move {
+        message_broker.actor_loop(mb_io).await
+    });
+    
+    // Spawn delivery_status reader for message-broker-actor:
+    let (mut mb_io2_i, mut mb_io2_o) = mb_io2.split();
+    tokio::spawn(async move {
+        while let Some(delivery_status) = mb_io2_i.recv().await {
+            log::debug!("{:?}", delivery_status);
+        }
+    });
+
+    loop {
+        let header = sub.next().await.unwrap().unwrap();
+        let block = fetch_block(header.number, &mut b_io2).await;
+        println!("BLOCK: {:?}", &block);
+        let payload = serde_json::to_string_pretty(&block).unwrap();
+        println!("{}", &payload);
+        mb_io2_o.send(ActorDirective::Input(payload)).await.unwrap();
+    }
     
     // let sub = client.subscribe_finalized_events().await.unwrap();
     // let events_decoder = client.events_decoder();
@@ -83,15 +112,10 @@ async fn main() {
     //     events_decoder
     // );
     
-    let mut message_broker = MessageBrokerActor::new();
-    let (mb_io1, mb_io2) = MessageBrokerActorIO::pair();
-    tokio::spawn(async move { message_broker.actor_loop(mb_io1).await });
-    let (mut mb_io2_i, mut mb_io2_o) = mb_io2.split();
-    tokio::spawn(async move {
-        while let Some(delivery_status) = mb_io2_i.recv().await {
-            log::debug!("{:?}", delivery_status);
-        }
-    });
+    
+    
+    // let mut task_queue = FuturesOrdered::new();
+    
     
     // loop {
     //     tokio::select! {
@@ -120,16 +144,16 @@ async fn main() {
 
 use substrate_subxt::ChainBlock;
 
-async fn fetch_block(number: <RuntimeT as System>::BlockNumber, b_io2: &mut BlockchainActorIOPair) -> ChainBlock<RuntimeT> {
-    b_io2.send(BlockchainActorInput::Input(
+async fn fetch_block(number: <RuntimeT as System>::BlockNumber, io: &mut BlockchainActorIOPair) -> ChainBlock<RuntimeT> {
+    io.send(BlockchainActorInput::Input(
         BlockchainActorInputData::GetBlockHash(number))).await.unwrap();
-    let block_hash = b_io2.recv().await.unwrap();
+    let block_hash = io.recv().await.unwrap();
     match block_hash {
         BlockchainActorOutput::GetBlockHash(maybe_hash) => {
             let hash = maybe_hash.expect("NO RPC ERROR").expect("EXISTENT BLOCK");
-            b_io2.send(BlockchainActorInput::Input(
+            io.send(BlockchainActorInput::Input(
                 BlockchainActorInputData::GetBlock(hash))).await.unwrap();
-            let block = b_io2.recv().await.unwrap();
+            let block = io.recv().await.unwrap();
             match block {
                 BlockchainActorOutput::GetBlock(maybe_block) => {
                     let block = maybe_block.expect("NO RPC ERROR").expect("EXISTENT BLOCK");
