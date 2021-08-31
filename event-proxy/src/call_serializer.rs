4eed623ce8c5dfb4cd15f9de36c8468af5618611
yaro <0623forbidden@gmail.com>
2021-08-31 12:59:31 +0300

    (event-proxy): Support calls from pallet_deip_assets

--- event-proxy/src/call_serializer.rs
+++ event-proxy/src/call_serializer.rs
@@ -8,6 +8,7 @@ impl Serialize for runtime::WrappedCall<<RuntimeT as DeipProposal>::Call> {
     {
         match &self.0 {
             // =============== Deip:
+            
             Call::Deip(pallet_deip::Call::create_project(
                                   is_private,
                                   external_id,
@@ -200,7 +201,9 @@ impl Serialize for runtime::WrappedCall<<RuntimeT as DeipProposal>::Call> {
                     },
                 }.serialize(serializer)
             },
+            
             // =============== DeipProposal:
+            
             Call::DeipProposal(pallet_deip_proposal::Call::propose(
                                       batch,
                                       external_id)) => {
@@ -225,7 +228,9 @@ impl Serialize for runtime::WrappedCall<<RuntimeT as DeipProposal>::Call> {
                     },
                 }.serialize(serializer)
             },
+            
             // =============== DeipOrg:
+            
             Call::DeipOrg(pallet_deip_org::Call::create(
                                  name,
                                  key_source)) => {
@@ -262,9 +267,270 @@ impl Serialize for runtime::WrappedCall<<RuntimeT as DeipProposal>::Call> {
                     },
                 }.serialize(serializer)
             },
+            
+            // =============== DeipAssets:
+            
+            Call::DeipAssets(pallet_deip_assets::Call::create_asset(
+                                 id,
+                                 admin,
+                                 max_zombies,
+                                 min_balance,
+                                 project_id)) => {
+                CallObject {
+                    module: "deip_assets",
+                    call: "create_asset",
+                    args: &DeipAssetsCreateAssetCallArgs {
+                        id,
+                        admin,
+                        max_zombies,
+                        min_balance,
+                        project_id
+                    },
+                }.serialize(serializer)
+            }
+            Call::DeipAssets(pallet_deip_assets::Call::destroy(
+                                 id,
+                                 zombies_witness)) => {
+                CallObject {
+                    module: "deip_assets",
+                    call: "destroy",
+                    args: &DeipAssetsDestroyCallArgs {
+                        id,
+                        zombies_witness
+                    },
+                }.serialize(serializer)
+            },
+            Call::DeipAssets(pallet_deip_assets::Call::issue_asset(
+                                 id,
+                                 beneficiary,
+                                 amount)) => {
+                CallObject {
+                    module: "deip_assets",
+                    call: "issue_asset",
+                    args: &DeipAssetsIssueAssetCallArgs {
+                        id,
+                        beneficiary,
+                        amount
+                    },
+                }.serialize(serializer)
+            },
+            Call::DeipAssets(pallet_deip_assets::Call::burn(
+                                 id,
+                                 who,
+                                 amount)) => {
+                CallObject {
+                    module: "deip_assets",
+                    call: "burn",
+                    args: &DeipAssetsBurnCallArgs {
+                        id,
+                        who,
+                        amount
+                    },
+                }.serialize(serializer)
+            },
+            Call::DeipAssets(pallet_deip_assets::Call::transfer(
+                                 id,
+                                 target,
+                                 amount)) => {
+                CallObject {
+                    module: "deip_assets",
+                    call: "transfer",
+                    args: &DeipAssetsTransferCallArgs {
+                        id,
+                        target,
+                        amount
+                    },
+                }.serialize(serializer)
+            },
+            Call::DeipAssets(pallet_deip_assets::Call::freeze(
+                                 id,
+                                 who)) => {
+                CallObject {
+                    module: "deip_assets",
+                    call: "freeze",
+                    args: &DeipAssetsFreezeCallArgs {
+                        id,
+                        who
+                    },
+                }.serialize(serializer)
+            },
+            Call::DeipAssets(pallet_deip_assets::Call::thaw(
+                                 id,
+                                 who)) => {
+                CallObject {
+                    module: "deip_assets",
+                    call: "thaw",
+                    args: &DeipAssetsThawCallArgs {
+                        id,
+                        who
+                    },
+                }.serialize(serializer)
+            },
+            Call::DeipAssets(pallet_deip_assets::Call::freeze_asset(
+                                 id)) => {
+                CallObject {
+                    module: "deip_assets",
+                    call: "freeze_asset",
+                    args: &DeipAssetsFreezeAssetCallArgs {
+                        id,
+                    },
+                }.serialize(serializer)
+            },
+            Call::DeipAssets(pallet_deip_assets::Call::thaw_asset(
+                                 id)) => {
+                CallObject {
+                    module: "deip_assets",
+                    call: "thaw_asset",
+                    args: &DeipAssetsThawAssetCallArgs {
+                        id,
+                    },
+                }.serialize(serializer)
+            },
+            Call::DeipAssets(pallet_deip_assets::Call::transfer_ownership(
+                                 id,
+                                 owner)) => {
+                CallObject {
+                    module: "deip_assets",
+                    call: "transfer_ownership",
+                    args: &DeipAssetsTransferOwnershipCallArgs {
+                        id,
+                        owner
+                    },
+                }.serialize(serializer)
+            },
+            Call::DeipAssets(pallet_deip_assets::Call::set_team(
+                                 id,
+                                 issuer,
+                                 admin,
+                                 freezer)) => {
+                CallObject {
+                    module: "deip_assets",
+                    call: "set_team",
+                    args: &DeipAssetsSetTeamCallArgs {
+                        id,
+                        issuer,
+                        admin,
+                        freezer
+                    },
+                }.serialize(serializer)
+            },
+            Call::DeipAssets(pallet_deip_assets::Call::set_max_zombies(
+                                 id,
+                                 max_zombies)) => {
+                CallObject {
+                    module: "deip_assets",
+                    call: "set_max_zombies",
+                    args: &DeipAssetsSetMaxZombiesCallArgs {
+                        id,
+                        max_zombies
+                    },
+                }.serialize(serializer)
+            },
+            Call::DeipAssets(pallet_deip_assets::Call::set_metadata(
+                                 id,
+                                 name,
+                                 symbol,
+                                 decimals)) => {
+                CallObject {
+                    module: "deip_assets",
+                    call: "set_metadata",
+                    args: &DeipAssetsSetMetadataCallArgs {
+                        id,
+                        name,
+                        symbol,
+                        decimals
+                    },
+                }.serialize(serializer)
+            },
             _ => serializer.serialize_u8(0)
         }
     }
+}
+
+#[derive(Serialize)]
+struct DeipAssetsSetMetadataCallArgs<A, B, C, D> {
+    id: A,
+    name: B,
+    symbol: C,
+    decimals: D,
+}
+
+#[derive(Serialize)]
+struct DeipAssetsSetMaxZombiesCallArgs<A, B> {
+    id: A,
+    max_zombies: B,
+}
+
+#[derive(Serialize)]
+struct DeipAssetsSetTeamCallArgs<A, B, C, D> {
+    id: A,
+    issuer: B,
+    admin: C,
+    freezer: D,
+}
+
+#[derive(Serialize)]
+struct DeipAssetsTransferOwnershipCallArgs<A, B> {
+    id: A,
+    owner: B,
+}
+
+#[derive(Serialize)]
+struct DeipAssetsThawAssetCallArgs<A> {
+    id: A,
+}
+
+#[derive(Serialize)]
+struct DeipAssetsFreezeAssetCallArgs<A> {
+    id: A,
+}
+
+#[derive(Serialize)]
+struct DeipAssetsThawCallArgs<A, B> {
+    id: A,
+    who: B,
+}
+
+#[derive(Serialize)]
+struct DeipAssetsFreezeCallArgs<A, B> {
+    id: A,
+    who: B,
+}
+
+#[derive(Serialize)]
+struct DeipAssetsTransferCallArgs<A, B, C> {
+    id: A,
+    target: B,
+    amount: C,
+}
+
+#[derive(Serialize)]
+struct DeipAssetsBurnCallArgs<A, B, C> {
+    id: A,
+    who: B,
+    amount: C,
+}
+
+#[derive(Serialize)]
+struct DeipAssetsIssueAssetCallArgs<A, B, C> {
+    id: A,
+    beneficiary: B,
+    amount: C,
+}
+
+#[derive(Serialize)]
+struct DeipAssetsDestroyCallArgs<A, B> {
+    id: A,
+    zombies_witness: B,
+}
+
+#[derive(Serialize)]
+struct DeipAssetsCreateAssetCallArgs<A, B, C, D, E> {
+    id: A,
+    admin: B,
+    max_zombies: C,
+    min_balance: D,
+    project_id: E,
 }
 
 #[derive(Serialize)]
