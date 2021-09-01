880032f35e17919ef755344583343b7c51577f8e
Georgy Shepelev <geo.shepelev@gmail.com>
2021-09-01 19:47:54 +0400

    add serialization to Asset for std builds

--- pallets/deip/src/asset.rs
+++ pallets/deip/src/asset.rs
@@ -1,6 +1,8 @@
 use crate::*;
 
 #[derive(Encode, Decode, Clone, RuntimeDebug, PartialEq, Eq)]
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "std", serde(rename_all = "camelCase"))]
 pub struct Asset<AssetId, AssetBalance> {
     pub id: AssetId,
     pub amount: AssetBalance,
