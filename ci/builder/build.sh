8876479169cda35f04d27a69e8e643fd64164e15
yaro <0623forbidden@gmail.com>
2021-10-04 14:54:26 +0300

    (devops): Refine build scripts

--- ci/builder/build.sh
+++ ci/builder/build.sh
@@ -4,18 +4,17 @@ set -e
 
 BUILDER_IMAGE=${BUILDER_IMAGE:-deip-rust-builder}
 
-REVISION=${REVISION?Please specify vcs REVISION}
 BUILD_SOURCE=${BUILD_SOURCE?Please specify BUILD_SOURCE directory}
 BUILD_CACHE=${BUILD_CACHE?Please specify BUILD_CACHE directory}
 BUILD_SCRIPT=${BUILD_SCRIPT?Please specify BUILD_SCRIPT file (relative to BUILD_SOURCE)}
 BUILD_WORKDIR=${BUILD_WORKDIR?Please specify BUILD_WORKDIR directory (relative to BUILD_SOURCE)}
 BINARY_NAME=${BINARY_NAME?Please specify BINARY_NAME (build artifact)}
-APP_IMAGE=${APP_IMAGE?Please specify APP_IMAGE tag}
+ARTIFACT=${ARTIFACT?Please specify build ARTIFACT lication}
 
 echo "+------------------------------------------------+"
 echo "|    Prepare builder-image                       |"
 echo "+------------------------------------------------+"
-docker build -f Dockerfile-builder -t "$BUILDER_IMAGE" .
+docker build -f Dockerfile -t "$BUILDER_IMAGE" .
 
 echo "+------------------------------------------------+"
 echo "|    Building...                                 |"
@@ -37,10 +36,4 @@ docker run --rm -ti -v "$BUILD_SOURCE":"$build_source" -v "$BUILD_CACHE":"$build
   "$BUILDER_IMAGE" ./"$BUILD_SCRIPT"
 
 artifact="$BUILD_CACHE/$cargo_target_dir/$build_mode/$BINARY_NAME"
-ctx="$BUILD_CACHE"/ctx/"$BINARY_NAME_$REVISION"
-mkdir -p ctx
-cp $artifact ctx
-docker build -f Dockerfile -t "$APP_IMAGE" \
-  --build-arg=ARTIFACT=$BINARY_NAME \
-  ctx
-rm -rf ctx
+cp $artifact $ARTIFACT
--- ci/deploy.sh
+++ ci/deploy.sh
@@ -9,28 +9,28 @@ export REVISION=$(git -C $BUILD_SOURCE rev-parse --short HEAD)
 
 cat /dev/null > .env
 
-LOCAL_REPO="localhost:5000"
+LOCAL_REPO="localhost"
 echo "LOCAL_REPO=$LOCAL_REPO" >> .env
 
 export BUILD_SCRIPT="event-proxy/build.sh"
 export BUILD_WORKDIR="event-proxy"
 export BINARY_NAME="event-proxy"
 export APP_IMAGE="$BINARY_NAME:$REVISION"
-cd ./builder && ./build.sh && cd .. && docker tag $APP_IMAGE "$LOCAL_REPO/$APP_IMAGE"
+./build_img.sh && docker tag $APP_IMAGE "$LOCAL_REPO/$APP_IMAGE"
 echo "EVENT_PROXY_IMG=$APP_IMAGE" >> .env
 
 export BUILD_SCRIPT="node/build.sh"
 export BUILD_WORKDIR="."
 export BINARY_NAME="node-template"
 export APP_IMAGE="$BINARY_NAME:$REVISION"
-cd ./builder && ./build.sh && cd .. && docker tag $APP_IMAGE "$LOCAL_REPO/$APP_IMAGE"
+./build_img.sh && docker tag $APP_IMAGE "$LOCAL_REPO/$APP_IMAGE"
 echo "BLOCKCHAIN_NODE_IMG=$APP_IMAGE" >> .env
 
 export BUILD_SCRIPT="event-proxy-client/build.sh"
 export BUILD_WORKDIR="event-proxy-client"
 export BINARY_NAME="event-proxy-client"
 export APP_IMAGE="$BINARY_NAME:$REVISION"
-cd ./builder && ./build.sh && cd .. && docker tag $APP_IMAGE "$LOCAL_REPO/$APP_IMAGE"
+./build_img.sh && docker tag $APP_IMAGE "$LOCAL_REPO/$APP_IMAGE"
 echo "EVENT_PROXY_CLIENT_IMG=$APP_IMAGE" >> .env
 
 docker-compose up -d --no-build
